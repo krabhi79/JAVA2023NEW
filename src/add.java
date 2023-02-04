21512403c7f82c4082eba04f1758c2ea6bf1c4bc
krabhi79 <krabhishek399@outlook.com>
2023-02-05 03:25:01 +0530

    add

---
+++ src/add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String []args){
+        int num1=15;
+        int num2=10;
+        int total= num1+num2;
+        System.out.println(total);
+
+    }
+}
