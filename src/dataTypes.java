01e18fc3dd8f1c7cc284f286c74acc14a276f7aa
krabhi79 <krabhishek399@outlook.com>
2023-02-05 12:20:18 +0530

    dataTypes

---
+++ src/dataTypes.java
@@ -0,0 +1,10 @@
+public class dataTypes {
+    public static void main(String []args){
+        //byte data = 127;
+        //short data = 129;
+        //long data = 1554948898;
+        //char data = 'a';
+        int data = 1457;
+        System.out.println(data);
+    }
+}
