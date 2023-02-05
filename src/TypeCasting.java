24cc985b9de7c15fc4bbf9ceb221c41e883076b5
krabhi79 <krabhishek399@outlook.com>
2023-02-05 21:29:22 +0530

    TypeCasting & Truncation

---
+++ src/TypeCasting.java
@@ -0,0 +1,17 @@
+public class TypeCasting {
+    static public void main(String []args){
+        int a = 45;
+        double b;
+        b=a;
+        System.out.println(b);
+        double num1=19.9;
+        int num2;
+        num2=(int)num1;
+        System.out.println(num2);
+        int n1=21;
+        int n2=4;
+        int result=n1/n2;
+        System.out.println(result);
+
+    }
+}
