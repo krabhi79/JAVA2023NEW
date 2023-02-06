b27d109960c8cd2b234482db5830205ddd3b77be
krabhi79 <krabhishek399@outlook.com>
2023-02-06 23:18:03 +0530

    Checking TypeCasting & Truncation

---
+++ src/intValue.java
@@ -0,0 +1,10 @@
+public class intValue {
+    public static void main(String []args){
+        int n1= 99;
+        int n2= 98;
+        double n3=99.5;
+        int sum= n1+n2+(int)n3;
+        System.out.println(sum);
+    }
+
+}
