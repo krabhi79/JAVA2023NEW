66be5bb63fe95d7a2ee8b69a766f89309aa761e9
krabhi79 <krabhishek399@outlook.com>
2023-02-17 21:57:03 +0530

    IncrementationDecrementation

---
+++ src/IncrementationDecrementation.java
@@ -0,0 +1,9 @@
+public class IncrementationDecrementation {
+    public static void main (String []args){
+        int a=6;
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+    }
+}
