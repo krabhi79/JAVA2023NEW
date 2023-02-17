bb0b5f1cbcaf9ebb912ccfbbce89dfb52eeef602
krabhi79 <krabhishek399@outlook.com>
2023-02-17 21:46:09 +0530

    ArithmeticOperators

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperators {
+    public static void main(String args[]){
+        int num1=50;
+        int num2=5;
+        // System.out.println(num1 + num2);
+       // System.out.println(num1 - num2);
+       // System.out.println(num1 * num2);
+      //  System.out.println(num1 / num2);
+      //  System.out.println(100 % 10);
+        System.out.println(2 % 12);
+    }
+
+}
