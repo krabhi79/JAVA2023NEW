48d8ad32dd1ed5c610a2d924c05210a3b11fca23
krabhi79 <krabhishek399@outlook.com>
2023-02-18 19:09:15 +0530

    AssignmentOperators

---
+++ src/AssignmentOperators.java
@@ -0,0 +1,16 @@
+public class AssignmentOperators {
+    public static void main(String [] args){
+       // int a=10;
+      //  a= a+5;
+        //System.out.println(a);
+      //  int n= 55;
+       // n=n-50;
+       // System.out.println(n);
+      //  int m= 99;
+        //m -= 90;
+       // System.out.println(m);
+      int s = 76;
+      s += 23;
+        System.out.println(s);
+    }
+}
