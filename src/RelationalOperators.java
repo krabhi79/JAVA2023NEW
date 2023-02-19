6ecf8806d067aff334f1534e2247695c3ac083d9
krabhi79 <krabhishek399@outlook.com>
2023-02-19 22:22:08 +0530

    RelationOperators

---
+++ src/RelationalOperators.java
@@ -0,0 +1,17 @@
+public class RelationalOperators {
+    public static void main(String [] args){
+        int a =20;
+        int b =20;
+        int c =40;
+       // System.out.println(a==b);
+       // System.out.println(c<b);
+       // System.out.println(a>c);
+        // System.out.println(c==a);
+       // System.out.println(c>b);
+      //  System.out.println(b<c);
+        //boolean result =a>=b;
+       // boolean result =b!=a;
+        boolean result =a!=c;
+        System.out.println(result);
+    }
+}
