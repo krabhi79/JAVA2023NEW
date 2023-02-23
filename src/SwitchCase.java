d2133d88e71b7ed83831a5da3b4db30b0142736c
krabhi79 <krabhishek399@outlook.com>
2023-02-24 00:05:58 +0530

    SwitchCase

---
+++ src/SwitchCase.java
@@ -0,0 +1,20 @@
+public class SwitchCase {
+    public static void main(String [] args){
+        int data = 6700;
+
+        switch (data)
+        {
+            case 600: System.out.println("case1");
+            break;
+            case 200: System.out.println("case2");
+            break;
+            case 100: System.out.println("case3");
+            break;
+            case 50: System.out.println("case4");
+            break;
+            case 400: System.out.println("case5");
+            break;
+            default: System.out.println("default");
+        }
+    }
+}
