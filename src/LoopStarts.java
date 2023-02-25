dd3657b2981db0738556f86209e3e37ca8c1eb39
krabhi79 <krabhishek399@outlook.com>
2023-02-25 19:16:22 +0530

    ForLoop

---
+++ src/LoopStarts.java
@@ -0,0 +1,8 @@
+public class LoopStarts {
+    public static void main(String[] args) {
+        for(int i=0 ; i<10 ; i++)
+            {
+                System.out.println("body of loop");
+            }
+        }
+    }
