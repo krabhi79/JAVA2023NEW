856e3ea71eb5c77fe3956eeeea2b82faa7bbd2b3
krabhi79 <krabhishek399@outlook.com>
2023-02-26 22:01:22 +0530

    DoWhileLoop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String [] args){
+        int j=0;
+        do {
+            System.out.println("Condition is placed outside the body.");
+           j++;
+           }while(j<5);
+    }
+}
