f081f9f8a5bbef228bc191f55733436f0de12b85
krabhi79 <krabhishek399@outlook.com>
2023-03-22 00:02:07 +0530

    1d Array

---
+++ src/Array1D.java
@@ -0,0 +1,14 @@
+public class Array1D {
+    public static void main(String[] args) {
+        int number[] ={1,2,3,12,6,4,98,97,8,5,48,89,4,89,5};
+        for (int i=0;i<=14;i++)
+        {
+            System.out.println(number[i]);
+        }
+        String names[]={"Abhishek","Avnish","Mashum"};
+        for (int j=0;j<=2;j++)
+        {
+            System.out.println(names[j]);
+        }
+    }
+}
