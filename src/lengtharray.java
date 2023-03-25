51f0da14a3d29a8bbe2e36e5bd500f5ac030efa7
krabhi79 <krabhishek399@outlook.com>
2023-03-25 13:16:34 +0530

    LengthArray

---
+++ src/lengtharray.java
@@ -0,0 +1,9 @@
+public class lengtharray {
+    public static void main(String[] args) {
+        int nums[][] = {{1, 2, 3, 4},
+                {1, 2, 3, 3},
+                {2, 5, 9}
+        };
+        System.out.println(nums[1].length);
+    }
+}
