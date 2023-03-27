fb002c764d1636169a9ef15b0951fe1ed20c9dc1
krabhi79 <krabhishek399@outlook.com>
2023-03-27 23:37:06 +0530

    ArrayOutOfBoundIndex

---
+++ src/ArrayOutOfBound.java
@@ -0,0 +1,14 @@
+public class ArrayOutOfBound {
+    public static void main(String[] args) {
+        int a []={5,9,79,945};
+//        for (int i=0;i<a.length;i++)
+//        {
+//            System.out.print( a[i] + " ");
+//        }
+//        System.out.print(" Hello Guys!!");
+        for (int n : a)
+        {
+            System.out.print(n + " ");
+        }
+    }
+}
