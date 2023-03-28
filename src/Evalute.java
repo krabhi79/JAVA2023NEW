403ab78327b2d253d99872d681c5529a0a73f241
krabhi79 <krabhishek399@outlook.com>
2023-03-28 22:00:08 +0530

    ArrayEvaluteQuestion

---
+++ src/Evalute.java
@@ -0,0 +1,19 @@
+class Evaluate
+
+{
+
+    public static void main(String args[])
+
+    {
+
+        int arr[] = new int[] {0,1,2,3,4,8,5,4,8};
+
+        int n = 5;
+
+        n = arr[arr[n] / 2];
+
+        System.out.println(arr[n] / 2);
+
+    }
+
+}
