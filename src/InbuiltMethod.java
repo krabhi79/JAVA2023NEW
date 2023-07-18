13cbf65e6d7ce462b909d7887ab12463c2935685
krabhi79 <krabhishek399@outlook.com>
2023-07-18 19:08:25 +0530

    InbuiltMethodOfString

---
+++ src/InbuiltMethod.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class InbuiltMethod {
+    public static void main(String[] args) {
+        String name="Java is a Programming language";
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.length());
+        System.out.println(name.lastIndexOf("a"));
+        System.out.println(name.indexOf("a"));
+        System.out.println(name.charAt(1));
+        System.out.println(name.substring(0,22));
+    }
+}
