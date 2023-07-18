d707a887bbba0e18555b31a28451ac2006feb2d8
krabhi79 <krabhishek399@outlook.com>
2023-07-18 18:23:08 +0530

    StringContenationUsingOperator

---
+++ src/StringContenationOperator.java
@@ -0,0 +1,8 @@
+public class StringContenationOperator {
+    public static void main(String[] args) {
+        String str= "Abhishek";
+        String str1= "Abhishek";
+        str = "Mash";
+        System.out.println(str+str1);
+    }
+}
