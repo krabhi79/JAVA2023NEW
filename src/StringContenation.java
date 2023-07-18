e065020bcc1aff5da731aacc0bdfcb66c1764d99
krabhi79 <krabhishek399@outlook.com>
2023-07-18 18:20:59 +0530

    StringContenationUsingConcatMethod

---
+++ src/StringContenation.java
@@ -0,0 +1,12 @@
+public class StringContenation {
+    public static void main(String[] args) {
+        String a1 = "Mash";
+        String a2 = a1.concat("Abhishek");
+        String a3 = new String("Mash");
+        a3=a3.concat("Abhishek");
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+
+    }
+}
